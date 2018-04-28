03e5de8226c50a51d52827adc7194e7141c31927
Bridger Sanders <35834184+Bridger-Sanders@users.noreply.github.com>
2018-04-28 12:43:57 -0700

    submitting hw-6

--- hw-6/readme.md
+++ hw-6/readme.md
@@ -1,3 +1,27 @@
-# Empty p5 sketch.
+Bridger Sanders, Section 50
 
-Please replace contents of this file with appropriate readme information after finishing your p5 sketch.
+["Sketch"](https://bridger-sanders.github.io/120-work/hw-6)
+
+# hw-5 response
+
+## Work Summary
+
+I had to work up the mustar the nerve to get my **** together, but generally I just look through the lesson plan and use it as reference. I look at the other students work to get ideas, they're my muse. 
+
+## Steps Taken
+
+- Trial and Error
+- Constantly switching between *sketch.js* and the correlated *index.html* after typing a line of code.
+- Refer back to examples for guidance
+
+## Issues & Solutions
+
+The planets were in grayscale for some reason when I attempted to use a variable with a color designation, so I manually put in the fill colors for the original rgb colors I wanted them to be.
+
+## What I Learned 
+
+Don't procrastinate or you will never see the light of day for roughly 12 hours straight per assignment.
+
+## Classmate Issues
+
+I have not helped my classmates, but that's due to my lack of enthusiasm. 
--- hw-6/sketch.js
+++ hw-6/sketch.js
@@ -1,7 +1,99 @@
-function setup(){
+ var sun = {
+     size: 0,
+ };
 
+ var star = {
+     x: 0,
+     y: 0,
+     size: 0,
+ };
+
+ var planetSize = 20;
+ 
+ var r = 0;
+ var g = 0;
+ var b = 0;
+
+function setup(){
+    createCanvas( 1366, 650 );
 }
 
 function draw(){
-    
+    //background black to yellow
+    r = map( mouseX, 126, 1366, 0, 255 );
+    g = map( mouseX, 126, 1366, 0, 255 );
+    background( r, g, 0 );
+
+    //random stars
+    star.x = random( 0, width );
+    star.y = random( 0, height );
+    star.size = random( 2, 4);
+    fill( 255 );
+    rect( star.x, star.y, star.size, star.size );
+
+    //sun
+    sun.size = random( ( planetSize * 109 ) - 10, planetSize * 109 );
+    noStroke();
+    fill( 255, 255, 0 );
+    ellipse( -900, height/2, sun.size, sun.size );
+
+    //space walls
+    let mercuryWall = width - 1100;
+    let venusWall = width - 1050;
+    let earthWall = width - 1000;
+    let marsWall = width - 950;
+    let jupiterWall = width - 775;
+    let saturnWall = width - 525;
+    let uranusWall = width - 350;
+    let neptuneWall = width -250;
+
+    //space constrains
+    let mec = constrain(mouseX, mercuryWall, width);
+    let vec = constrain(mouseX, venusWall, width);
+    let eac = constrain(mouseX, earthWall, width);
+    let mac = constrain(mouseX, marsWall, width);
+    let juc = constrain(mouseX, jupiterWall, width);
+    let sac = constrain(mouseX, saturnWall, width);
+    let urc = constrain(mouseX, uranusWall, width);
+    let nec = constrain(mouseX, neptuneWall, width);
+
+    //jupiter
+    noStroke();
+    fill( 233, 150, 122 );
+    ellipse( juc, mouseY, planetSize * 11.21, planetSize * 11.21 );
+
+    //saturn
+    noStroke();
+    fill( 255, 160, 122 );
+    ellipse( sac, mouseY, planetSize * 9.45, planetSize * 9.45 );
+
+    //uranus
+    noStroke();
+    fill( 65, 105, 225 );
+    ellipse( urc, mouseY, planetSize * 4.01, planetSize * 4.01 );
+       
+    //neptune
+    noStroke();
+    fill( 72, 61, 139 );
+    ellipse( nec, mouseY, planetSize * 3.88, planetSize * 3.88 );
+
+    //earth
+    noStroke();
+    fill( 25, 25, 112 );
+    ellipse( eac, mouseY, planetSize, planetSize );
+
+    //venus
+    noStroke();
+    fill( 255, 140, 0 );
+    ellipse( vec, mouseY, planetSize * 0.949 , planetSize * 0.949 );
+
+    //mars
+    noStroke();
+    fill( 165, 42, 42 );
+    ellipse( mac, mouseY, planetSize * 0.532, planetSize * 0.532 );
+
+    //mercury
+    noStroke();
+    fill( 210, 180, 140 );
+    ellipse( mec, mouseY, planetSize * 0.383, planetSize * 0.383 );
 }
