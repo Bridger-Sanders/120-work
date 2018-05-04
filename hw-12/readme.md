1a54f89400ff1b18ba0dd90ff45ad79f44058e01
Bridger Sanders <35834184+Bridger-Sanders@users.noreply.github.com>
2018-05-04 01:27:35 -0600

    committing hw-12

--- hw-12/readme.md
+++ hw-12/readme.md
@@ -6,7 +6,7 @@ Bridger Sanders, Section 50
 
 ## Work Summary
 
-Came up with the idea to make a game similar to ping pong, then I started coding what I knew how to do. Then I worked on getting the ball to bounce off the paddle.
+Came up with the idea to make a game similar to ping pong, then I started coding what I knew how to do. First was the visual parts, black background, white ball, and white paddle. Next was getting the ball to move and bounce off the edges of the canvas, along with the paddle being tracked to the mouse. Then I worked on getting the ball to bounce off the paddle, that took awhile. Finally I finished up by having the ball reset every time it went below the paddle or in this case when the ball hits the bottom of the canvas.
 ## Steps Taken
 
 - Trial and Error
@@ -15,15 +15,17 @@ Came up with the idea to make a game similar to ping pong, then I started coding
 - Reading the *Getting Started with p5.js* book
 - Checking the p5.js reference page
 - Watching the videos on the module
+- Went to w3schools.com
+- Accessed happycoding.io for reference on collision detection
 
 ## Issues & Solutions
 
 Still having trouble how I'm suppose to lay things out with classes, constructors, `this.`, functions, etc.
-Getting the ball to bounce off the paddle is difficult.
+Getting the ball to bounce off the paddle was difficult.
 
 ## What I Learned 
 
-I learned that I have a lot to learn, even getting this far.
+I found out that I have a lot to learn about collisions, even after getting this far.
 
 ## Classmate Issues
 
--- hw-12/sketch.js
+++ hw-12/sketch.js
@@ -1,55 +1,56 @@
-var paddle;
-var ball;
+var pong;
+
 function setup(){
 createCanvas(windowWidth, 650);
-paddle = new Paddle();
-ball = new Ball();
+pong = new Pong();
+noCursor();
 }
 
 function draw(){
 background(0);
-paddle.show();
-paddle.glide();
-ball.show();
-ball.move();
-ball.bounce();
+pong.show();
+pong.glide();
+pong.appear();
+pong.move();
+pong.bounce();
 }
 
-class Paddle {
+class Pong {
   constructor() {
+    //paddle info
     this.paddle_x_axis = 0;
     this.paddle_y_axis = height - 25;
     this.paddle_size_width = 100;
     this.paddle_size_height = 10;
     this.paddle_barrier_right = width - 100;
     this.paddle_barrier_left = 0;
+    //paddle visual
     this.show = function () {
       fill(255);
       noStroke();
       rect(this.paddle_x_axis, this.paddle_y_axis, 
         this.paddle_size_width, this.paddle_size_height);
     };
+    //paddle position and tracking
     this.glide = function () {
       this.paddle_x_axis = constrain(mouseX - (this.paddle_size_width / 2), 
       this.paddle_barrier_left, this.paddle_barrier_right);
     };
-  };
-}
-
-class Ball {
-  constructor() {
-    var five = [5, -5]
+    //ball info
+    var fives = [-5, 5]
     this.ball_x_axis = width / 2;
     this.ball_y_axis = height / 2;
     this.ball_size = 10;
-    this.ball_move_x_axis = random(five);
+    this.ball_move_x_axis = random(fives);
     this.ball_move_y_axis = 5;
     this.ball_radius = this.ball_size / 2;
-    this.show = function (){
+    //ball visual
+    this.appear = function (){
       fill(255);
       noStroke();
       ellipse(this.ball_x_axis, this.ball_y_axis, this.ball_size, this.ball_size);
     };
+    //ball movement and restrictions
     this.move = function (){
       this.ball_x_axis += this.ball_move_x_axis;
       this.ball_y_axis += this.ball_move_y_axis;
@@ -63,21 +64,23 @@ class Ball {
           this.ball_x_axis = abs(this.ball_move_x_axis);
         } 
         else if (this.ball_y_axis >= height) {
-          this.ball_move_y_axis *= -1;
-          this.ball_y_axis = height - abs(this.ball_move_y_axis);
+          this.ball_x_axis = width / 2; 
+          this.ball_y_axis = height / 2;
+          this.ball_move_x_axis = random(fives);
+          //this.ball_move_y_axis *= -1;
+          //this.ball_y_axis = height - abs(this.ball_move_y_axis);
         } 
         else if (this.ball_y_axis <= 0) {
           this.ball_move_y_axis *= -1;
           this.ball_y_axis = abs(this.ball_move_y_axis);
         };
+    //collision between ball and paddle
     this.bounce = function (){
       if (this.ball_x_axis + this.ball_size > this.paddle_x_axis && 
           this.ball_x_axis < this.paddle_x_axis + this.paddle_size_width && 
-          this.ball_y_axis + this.ball_size + this.ball_move_y_axis > 
-      this.paddle_y_axis && 
-          this.ball_y_axis + this.ball_move_y_axis < this.paddle_y_axis + 
-      this.paddle_size_height) {
-        this.ball_move_y_axis *= -1;
+          this.ball_y_axis + this.ball_size + this.ball_move_y_axis > this.paddle_y_axis && 
+          this.ball_y_axis + this.ball_move_y_axis < this.paddle_y_axis + this.paddle_size_height) {
+          this.ball_move_y_axis *= -1;
         };
       };
     };
